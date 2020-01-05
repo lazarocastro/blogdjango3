30401fe191dd3ded2daba137b52a65667270f58d
Lazaro A. S. Castro <lazarocastro@gmail.com>
2020-01-05 18:39:57 -0300

    Read posts from database

--- blog/website/views.py
+++ blog/website/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def hello_blog(request):
     lista = [
-        'Django',
-        'Python',
-        'Git',
-        'Html',
-        'Banco de dados',
-        'Linux',
-        'Nginx',
-        'Uwsgi',
-        'Systemctl'
+        'Django', 'Python', 'Git', 'Html',
+        'Banco de dados', 'Linux', 'Nginx',
+        'Uwsgi', 'Systemctl'
     ]
-    data = {'name': 'Curso de Django 3', 'lista_tecnologias': lista}
+
+    list_posts = Post.objects.all()
+
+    data = {
+        'name': 'Curso de Django 3',
+        'lista_tecnologias': lista,
+        'posts': list_posts,
+    }
     return render(request, 'index.html', data)
